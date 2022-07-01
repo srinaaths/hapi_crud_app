662f65a2783be5ab9aee978473c8afe49fdbb9fc
srinaaths <srinaath.s@surveysparrow.com>
2022-07-01 14:56:08 +0530

    updated reply messages & status codes - works fine with node v14

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -10,7 +10,7 @@ const getAllMovies = (request, reply) => {
     reply(moviesList).code(200)
 }
 
-const postMovie = (request, reply) => {
+const postMovie = async (request, reply) => {
     const pathToWrite = path.join(__dirname, '..', 'data.json')
     const payload = request.payload;
     const size = moviesList.movies.length;
@@ -22,46 +22,59 @@ const postMovie = (request, reply) => {
             console.log(err.message);
         console.log('success');
     })
-    reply().code(201)
+    await reply('created').code(201)
 }
 
-const updateMovie = (request, reply) => {
+const updateMovie = async(request, reply) => {
     const id = request.params.id;
     const payload = request.payload;
     payload.id = Number(request.params.id);
     let indexToUpdate;
-    for(let i = 0; i < moviesList.movies.length; ++i) {
-        if(moviesList.movies[i].id == id) {
+    let flag = false;
+    for (let i = 0; i < moviesList.movies.length; ++i) {
+        if (moviesList.movies[i].id == id) {
             indexToUpdate = i;
+            flag = true;
         }
     }
-    moviesList.movies.splice(indexToUpdate, 1, payload);
-    const pathToWrite = path.join(__dirname, '..', 'data.json')
-    fs.writeFile(pathToWrite, JSON.stringify(moviesList), (err) => {
-        if (err)
-            console.log(err.message);
-        console.log('success');
-    })
-    reply.close();
+    console.log(flag);
+    if (!flag) {
+        reply('no movie found to update').code(200);
+    }
+    else {
+        moviesList.movies.splice(indexToUpdate, 1, payload);
+        const pathToWrite = path.join(__dirname, '..', 'data.json')
+        fs.writeFile(pathToWrite, JSON.stringify(moviesList), (err) => {
+            if (err)
+                console.log(err.message);
+            console.log('success');
+        })
+        await reply('updated').code(200);
+    }
 }
 
 const deleteMovie = (request, reply) => {
     const id = request.params.id;
     let indexToDelete;
-    for(let i = 0; i < moviesList.movies.length; ++i) {
-        if(moviesList.movies[i].id == id) {
+    let flag = false;
+    for (let i = 0; i < moviesList.movies.length; ++i) {
+        if (moviesList.movies[i].id == id) {
             indexToDelete = i;
+            flag = true;
         }
     }
-    moviesList.movies.splice(indexToDelete, 1);
-    const pathToWrite = path.join(__dirname, '..', 'data.json')
-    fs.writeFile(pathToWrite, JSON.stringify(moviesList), (err) => {
-        if (err)
-            console.log(err.message);
-        console.log('success');
-        reply('finished').code(200);
-    })
-    reply.close();
+    if (!flag)
+        reply('no movie found to delete');
+    else {
+        moviesList.movies.splice(indexToDelete, 1);
+        const pathToWrite = path.join(__dirname, '..', 'data.json')
+        fs.writeFile(pathToWrite, JSON.stringify(moviesList), (err) => {
+            if (err)
+                console.log(err.message);
+            console.log('success');
+        })
+        reply('deleted').code(204);
+    }
 }
 
-module.exports = {handlerFunction, getAllMovies, postMovie, updateMovie, deleteMovie}
+module.exports = { handlerFunction, getAllMovies, postMovie, updateMovie, deleteMovie }
