3b74806755d183dc72cd7b962d77e7f147d28aca
srinaaths <srinaath.s@surveysparrow.com>
2022-07-04 13:08:53 +0530

    validation updated

--- validation.js
+++ validation.js
@@ -1,11 +1,16 @@
 const Joi = require('joi')
 
 const movieIdValidator = Joi.object({
-    id: Joi.number().required(),
+    id: Joi.number().min(1).max(1000000).required(),
 })
 
 const movieNameValidator = Joi.object({
-    name: Joi.string().alphanum()
+    name: Joi.string().alphanum().min(2).max(30)
+})
+
+const movieValidator = Joi.object({
+    id: Joi.number(),
+    name: Joi.string().min(2)
 })
 
 module.exports = {movieIdValidator, movieNameValidator}
